bb708c8a11dff38fc786b415183f5b612a6e471d
Ethan Blackwood <ethan@cs.dartmouth.edu>
2015-05-30 02:18:59 -0400

    modified stim code for mwpi

--- code/+MWPI/+Stim/Stimulus.m
+++ code/+MWPI/+Stim/Stimulus.m
@@ -1,12 +1,14 @@
-function [im,b] = Stimulus(shp,varargin)
-% GO.Stim.Stimulus
+function [im,b] = Stimulus(shp,colFore,colBack,varargin)
+% MWPI.Stim.Stimulus
 % 
 % Description:	render a stimulus
 % 
-% Syntax:	[im,b] = GO.Stim.Stimulus(shp,<options>)
+% Syntax:	[im,b] = MWPI.Stim.Stimulus(shp,colFore,colBack<options>)
 % 
 % In:
 %	shp		- the stimulus shape (1:4)
+%	colFore - foreground color (RGB)
+%	colBack - background color (RGB)
 %	<options>:
 %		map:		([1;3;2;4]) a 4x1 array specifying a mapping from (1:4) to
 %					the actual stimulus, ordered as (R1,R2,P1,P2)
@@ -17,7 +19,7 @@ function [im,b] = Stimulus(shp,varargin)
 % 	im	- the stimulus image
 %	b	- the stimulus mask
 % 
-% Updated: 2013-09-24
+% Updated: 2015-05-30 for MWPI
 % Copyright 2013 Alex Schlegel (schlegel@gmail.com).  This work is licensed
 % under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
 % License.
@@ -29,13 +31,13 @@ opt	= ParseArgs(varargin,...
 
 switch opt.map(shp)
 	case 1%R1
-		[im,b]	= GO.Stim.Rect(1,opt.rotation,opt.flip);
+		[im,b]	= MWPI.Stim.Rect(1,opt.rotation,opt.flip,colFore,colBack);
 	case 2%R2
-		[im,b]	= GO.Stim.Rect(2,opt.rotation,opt.flip);
+		[im,b]	= MWPI.Stim.Rect(2,opt.rotation,opt.flip,colFore,colBack);
 	case 3%P1
-		[im,b]	= GO.Stim.Polar(1,opt.rotation,opt.flip);
+		[im,b]	= MWPI.Stim.Polar(1,opt.rotation,opt.flip,colFore,colBack);
 	case 4%P2
-		[im,b]	= GO.Stim.Polar(2,opt.rotation,opt.flip);
+		[im,b]	= MWPI.Stim.Polar(2,opt.rotation,opt.flip,colFore,colBack);
 	otherwise
 		error('wtf?');
 end
