abc8c44c4d8950a9df4dfbe988981361ab0c77a1
Ethan Blackwood <ethan@cs.dartmouth.edu>
2015-12-16 23:31:49 -0500

    moved callback function to seperate scope to prevent saving the workspace with function handles

--- code/@MWPI/Practice.m
+++ code/@MWPI/Practice.m
@@ -18,11 +18,9 @@ mwpi.sParam = MWPI.CalcParams('practice', true);
 mwpi.sParam = structfun(@(f) f*0, mwpi.sParam, 'uni', false);
 
 mwpi.nCorrect = 0;
-res = [];
+exp.Info.Set('mwpi','currRes', []);
 
 % get some params
-contOffset	= MWPI.Param('text', 'contOffset');
-tPreBlock	= MWPI.Param('practice','tPreBlock');
 indClass	= MWPI.Param('stim', 'class');
 curve		= MWPI.Param('curve');
 estimate	= MWPI.Param('practice', 'startLevel');
@@ -57,7 +55,7 @@ exp.Window.Flip;
 ListenChar(0);
 
 % save results
-sRun.res = res;
+sRun.block = exp.Info.Get('mwpi','currRes');
 
 sRuns = exp.Info.Get('mwpi','run');
 if isempty(sRuns)
@@ -66,6 +64,8 @@ else
 	sRuns(kRun) = sRun;
 end
 exp.Info.Set('mwpi','run',sRuns);
+exp.Info.Unset('mwpi','currRes');
+
 exp.Info.Set('mwpi','assessment', assess);
 exp.Info.Set('mwpi','param', mwpi.sParam);
 exp.Info.AddLog('Results saved.');
@@ -73,59 +73,98 @@ exp.Info.AddLog('Results saved.');
 exp.Subject.Set('assessment', assess);
 exp.Subject.AddLog('Ability assessment saved to subject info.');
 
-%---------------------------------------------------------------------%
-	function bCorrect = OneBlock(d, sTaskParam)
-		% Do a single block, including the preceding rest period.
-		% This is the callback function for the subject.assess object.
-		%
-		% In:
-		%	d:			difficulty of this block's wm / test stimulus
-		%	sTaskParam: parameter struct, with additional fields from
-		%				GenParamStruct
-		%
-		% Out:
-		%	bCorrect:	indicates whether the subject got the block correct
-		%	
+%--------------------------------------------------------------------------%
+	function sTaskParam = GenParamStruct(kTask, ~, kProbeTotal)
+		% generate a parameter struct for a random block of type kTask
+		% (param callback function for subject.assess/Run)
 		
-		kBlock = sTaskParam.kProbeTotal;
+		sTaskParam.wClass = kTask;
 		
-		% rest and generate new textures
-		arrAbility = vertcat(sTaskParam.estimate(:).ability);
-		DoRest(PTB.Now, tPreBlock, d, arrAbility, sTaskParam);
+		sTaskParam.vClass	= randFrom(indClass);
+		sTaskParam.dClass	= randFrom(indClass);
+		sTaskParam.cue		= randFrom(1:2);
+		sTaskParam.posMatch = randFrom(1:4);
 		
-		% write a log message
-		exp.AddLog(['starting block ' num2str(kBlock)]);
+		sTaskParam.lClass = conditional(sTaskParam.cue == 1, sTaskParam.wClass, sTaskParam.dClass);
+		sTaskParam.rClass = conditional(sTaskParam.cue == 2, sTaskParam.wClass, sTaskParam.dClass);
 		
-		% run the block
-		newRes = mwpi.Block(kRun, kBlock);
-		bCorrect = newRes.bCorrect;
-		
-		% save results
-		if isempty(res)
-			res = newRes;
-		else
-			res(end+1) = newRes;
-		end
-				
-		% pause before continue prompt
-		tPause = MWPI.Param('practice', 'tFbPause');
-		tNow = PTB.Now;
-		while PTB.Now < tNow + tPause
-			exp.Scheduler.Wait(PTB.Scheduler.PRIORITY_HIGH);
+		% update cumulative record of parameters
+		cParam = fieldnames(mwpi.sParam);
+		for i = 1:numel(cParam)
+			mwpi.sParam.(cParam{i})(kProbeTotal) = sTaskParam.(cParam{i});
 		end
 		
-		% add continue prompt
-		fbTexture = conditional(newRes.bCorrect, 'testYes', 'testNo');
-		strContinue = 'Press any key to continue.';
-		exp.Show.Text(strContinue, [0,contOffset], 'window', fbTexture);
-		exp.Show.Texture(fbTexture);
-		exp.Window.Flip;
-		
-		% wait for a key to be pressed
-		while ~exp.Input.Down('any')
-			exp.Scheduler.Wait(PTB.Scheduler.PRIORITY_CRITICAL);
-		end
+		% add additional fields to pass
+		sTaskParam.mwpi = mwpi;
+		sTaskParam.kRun = kRun;
 	end
+%------------------------------------------------------------------------%
+end
+
+%---------------------------------------------------------------------%
+function bCorrect = OneBlock(d, sTaskParam)
+% Do a single block, including the preceding rest period.
+% This is the callback function for the subject.assess object.
+% Note: this function must be outside of the scope of the main Practice
+% function, in order to avoid saving a huge workspace along with the
+% function handle. Necessary pointers are passed within sTaskParam.
+%
+% In:
+%	d:			difficulty of this block's wm / test stimulus
+%	sTaskParam: parameter struct, with additional fields from
+%				GenParamStruct
+%
+% Out:
+%	bCorrect:	indicates whether the subject got the block correct
+%
+
+mwpi = sTaskParam.mwpi;
+exp  = mwpi.Experiment;
+
+kBlock = sTaskParam.kProbeTotal;
+kRun   = sTaskParam.kRun;
+
+% rest and generate new textures
+arrAbility = vertcat(sTaskParam.estimate(:).ability);
+tPreBlock	= MWPI.Param('practice','tPreBlock');
+DoRest(PTB.Now, tPreBlock, d, arrAbility, sTaskParam);
+
+% write a log message
+exp.AddLog(['starting block ' num2str(kBlock)]);
+
+% run the block
+newRes = mwpi.Block(kRun, kBlock);
+bCorrect = newRes.bCorrect;
+
+% save results
+res = exp.Info.Get('mwpi','currRes');
+if isempty(res)
+	res = newRes;
+else
+	res(end+1) = newRes;
+end
+exp.Info.Set('mwpi','currRes',res);
+
+% pause before continue prompt
+tPause = MWPI.Param('practice', 'tFbPause');
+tNow = PTB.Now;
+while PTB.Now < tNow + tPause
+	exp.Scheduler.Wait(PTB.Scheduler.PRIORITY_HIGH);
+end
+
+% add continue prompt
+fbTexture = conditional(newRes.bCorrect, 'testYes', 'testNo');
+strContinue = 'Press any key to continue.';
+contOffset	= MWPI.Param('text', 'contOffset');
+exp.Show.Text(strContinue, [0,contOffset], 'window', fbTexture);
+exp.Show.Texture(fbTexture);
+exp.Window.Flip;
+
+% wait for a key to be pressed
+while ~exp.Input.Down('any')
+	exp.Scheduler.Wait(PTB.Scheduler.PRIORITY_CRITICAL);
+end
+
 %---------------------------------------------------------------------%
 	function DoRest(tStart, tWait, dNext, arrAbilityNext, sTaskParam)
 		% tStart, tWait in ms
@@ -139,24 +178,4 @@ exp.Subject.AddLog('Ability assessment saved to subject info.');
 		end
 	end
 %---------------------------------------------------------------------%
-	function sTaskParam = GenParamStruct(kTask, ~, kProbeTotal)
-		% generate a parameter struct for a random block of type kTask
-		% (param callback function for subject.assess/Run)
-
-		sTaskParam.wClass = kTask;
-
-		sTaskParam.vClass	= randFrom(indClass);
-		sTaskParam.dClass	= randFrom(indClass);
-		sTaskParam.cue		= randFrom(1:2);
-		sTaskParam.posMatch = randFrom(1:4);
-
-		sTaskParam.lClass = conditional(sTaskParam.cue == 1, sTaskParam.wClass, sTaskParam.dClass);
-		sTaskParam.rClass = conditional(sTaskParam.cue == 2, sTaskParam.wClass, sTaskParam.dClass);
-		
-		% update cumulative record of parameters
-		cParam = fieldnames(mwpi.sParam);
-		for i = 1:numel(cParam)
-			mwpi.sParam.(cParam{i})(kProbeTotal) = sTaskParam.(cParam{i});
-		end
-	end
 end
