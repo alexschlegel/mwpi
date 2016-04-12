9605e79233a5dacc024adf57e361f4240a1e13de
Ethan Blackwood <ethan@cs.dartmouth.edu>
2016-04-12 14:09:34 -0400

    delete

--- notes/timing_analysis/extract_triggers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-codes=(16mar16eb 16mar16kh 05apr16ph 02apr16gd 05apr16gl)
-
-for code in ${codes[@]}
-do
-	grep 'scanner\sTR' $code.log | gawk -F'[[:blank:]/]*' '{printf "%d, %.3f;\n", $4, $1}' > ${code}_triggers.csv
-done
-			
