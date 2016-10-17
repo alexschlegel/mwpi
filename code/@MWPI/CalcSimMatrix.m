b21bc5af5c04fc192cec969442c11eb450475a8d
Ethan Blackwood <ethanbb@comcast.net>
2016-10-17 16:18:24 -0400

    updated CalcSimMatrix

--- code/@MWPI/CalcSimMatrix.m
+++ code/@MWPI/CalcSimMatrix.m
@@ -9,22 +9,22 @@ function sm = CalcSimMatrix(res)
 % In:
 %	res	- PTBIFO.mwpi.simRes
 % 
-% Updated:	2016-03-01
+% Updated:	2016-10-17
 % Copyright 2016 Alex Schlegel (schlegel@gmail.com). This work is licensed
 % under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
 % License.
-kClass	= unique(res.sampleClass);
+kClass	= unique([res.sampleClass]);
 nClass	= numel(kClass);
 
 sm	= zeros(nClass);
 
 %score for rank 1, 2, 3, 4
-simScore	= [4 4 0 0];
+simScore	= [3 1 -1 -3];
 
-nTrial	= numel(res.sampleClass);
+nTrial	= numel([res.sampleClass]);
 for kT=1:nTrial
-	kSample		= res.sampleClass(kT);
-	kFlanker	= res.classesInOrderOfSimilarity{kT};
+	kSample		= res(kT).sampleClass;
+	kFlanker	= res(kT).classesInOrderOfSimilarity;
 	
 	sm(kSample,kFlanker)	= sm(kSample,kFlanker) + simScore;
 end
