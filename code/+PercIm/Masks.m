5d13df9936345c1fb1ddf13ee3319f519c3a22c9
Ethan Blackwood <ethan@cs.dartmouth.edu>
2016-10-17 21:38:21 -0400

    added Masks file, made CalcSimMatrix more usable

---
+++ code/+PercIm/Masks.m
@@ -0,0 +1,35 @@
+function s = Masks()
+% PercIm.Masks
+% 
+% Description:	get a struct of mask names
+% 
+% Syntax:	s = PercIm.Masks()
+% 
+% Updated: 2015-03-23 as MWL.Masks
+% Copyright 2015 Alex Schlegel (schlegel@gmail.com).  This work is licensed
+% under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+% License.
+s.ci	=	{
+				'dlpfc'
+				'fef'
+				'ppc'
+				'pcu'
+				'loc'
+				'occ'
+			};
+
+s.motor	=	{
+				'pmv'
+				'pmd'
+				'sma'
+				'pre_sma'
+				'primary_motor'
+				'somatosensory'
+				'cerebellum'
+			};
+
+s.all	= [s.motor; s.ci];
+
+sOrig	= s;
+s.left	= structtreefun(@(c) cellfun(@(str) [str '_left'],c,'uni',false),sOrig);
+s.right	= structtreefun(@(c) cellfun(@(str) [str '_right'],c,'uni',false),sOrig);
--- code/@MWPI/CalcSimMatrix.m
+++ code/@MWPI/CalcSimMatrix.m
@@ -1,18 +1,40 @@
-function sm = CalcSimMatrix(res)
+function cSM = CalcSimMatrix(varargin)
 % CalcSimMatrix
 % 
-% Description:	calculate an empirical similarity matrix based on the SimTest
-%				results
+% Description:	calculate empirical similarity matrices based on the SimTest
+%				results for a set of subjects
 % 
-% Syntax: sm = CalcSimMatrix(res)
+% Syntax: sm = CalcSimMatrix([code]=(all fmri))
 % 
-% In:
-%	res	- PTBIFO.mwpi.simRes
+% In: 
+%	code - cell of session codes to operate on
+%
+% Out:
+%	cSM - cell of similarity matrices for the given codes.
 % 
 % Updated:	2016-10-17
 % Copyright 2016 Alex Schlegel (schlegel@gmail.com). This work is licensed
 % under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
 % License.
+
+% get session codes
+if nargin == 0
+	cCode = getfield(PercIm.SubjectInfo,'code','fmri');
+else
+	cCode = ForceCell(varargin{1});
+end
+
+% get similarity test results
+sPTBIFO = cellfun(@(code) getfield(load(PathUnsplit(strDirData, code, 'mat')),'PTBIFO'), cCode);
+cSimRes = arrayfun(@(s) s.mwpi.simRes, sPTBIFO, 'uni', false);
+
+% calculate matrices
+cSM = cellfun(@CalcOneMatrix, cSimRes, 'uni', false);
+
+end
+
+function sm = CalcOneMatrix(res)
+
 kClass	= unique([res.sampleClass]);
 nClass	= numel(kClass);
 
@@ -34,3 +56,4 @@ end
 
 %normalize
 	sm	= normalize(sm,'min',0,'max',100);
+end
