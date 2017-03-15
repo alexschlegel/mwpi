3df05a88163e2afebfb3afb718417ff8fad0a944
Ethan Blackwood <ethan@cs.dartmouth.edu>
2017-03-15 17:34:51 -0400

    Add fixation task criterion to ClassificationInfo

--- code/+PercIm/ClassificationInfo.m
+++ code/+PercIm/ClassificationInfo.m
@@ -20,6 +20,9 @@ function s = ClassificationInfo(varargin)
 %					less than this if the number of TRs per block is less
 %					than offset + maxlen.
 %
+%   fixation_threshold: (0.85) the minimum mean fixation task accuracy per run required
+%          to include a run in the correct results.
+%
 %		fcorrect:   (<no correction>) the handle to a function that
 %					performs manual corrections to the labels and chunks of
 %					each run. An fcorrect function is specified as follows:
@@ -31,7 +34,7 @@ function s = ClassificationInfo(varargin)
 %						targetIn:	a target array that has not been manually corrected
 %						targetOut:  the corrected version of targetIn
 %						eventIn:	an event matrix that has not been manually corrected
-%						eventOut:	the corrected version of eventIn, 
+%						eventOut:	the corrected version of eventIn,
 %									or empty array if eventIn was empty.
 %
 % Out:
@@ -48,13 +51,14 @@ function s = ClassificationInfo(varargin)
 			'ifo'		, []					, ...
 			'offset'	, 0						, ...
 			'maxlen'	, 4						, ...
+      'fixation_threshold', 0.85, ...
 			'fcorrect'	, @(s,r,t,e) deal(t,e)	  ...
 			);
-		
+
 	if isempty(opt.ifo)
 		opt.ifo = PercIm.SubjectInfo;
 	end
-		
+
 	if isempty(opt.session)
 		cSession	= opt.ifo.code.fmri;
 	else
@@ -73,21 +77,21 @@ end
 
 function [sResult, bError] = LoadInfo(strSession, opt)
 	global strDirData;
-	
+
 	bError = false;
-		
+
 	strPathSession	= PathUnsplit(strDirData,strSession,'mat');
 	cScheme = opt.ifo.cScheme;
-	
+
 	if FileExists(strPathSession)
 		sSession = getfield(load(strPathSession), 'PTBIFO');
-		
+
 		PIParam = MWPI.Param;
-		
+
 		%%% Part 1: per-block parameters and results %%%
-		
+
 		% get stimulus ids and bCorrects, which are all in the same place %
-		
+
 		% matrix of field names vs. what they're called in the struct
 		cField = [ [cScheme; 'correct'], {
 											'vClass'
@@ -97,75 +101,79 @@ function [sResult, bError] = LoadInfo(strSession, opt)
 										}
 				 ];
 		nField = size(cField,1);
-		
+
 		sRes = sSession.mwpi.run;
-		
+
 		sBlock = struct;
 		for kF=1:nField
 			strFieldIn	= cField{kF,2};
 			strFieldOut	= cField{kF,1};
-			
+
 			x = arrayfun(@(run) [run.res.(strFieldIn)], sRes, 'uni',false);
 			x = vertcat(x{:});
-			
+
 			sBlock.(strFieldOut) = x;
 		end
-		
+
+    % add fixation task average accuracy (per-run mean)
+    sBlock.fixationAcc = cellnestflatten(arrayfun(@(run) repmat(run.meanFracFixationCorrect,length(run.res),1), 'uni', false));
+    sBlock.correct = sBlock.correct & (sBlock.fixationAcc >= opt.fixation_threshold);
+
 		% response time %
 		% stored values are already relative to test screen, in units of TR
 		x	= arrayfun(@(run) arrayfun(@(block) block.test.tResponse, ...
 			run.res, 'uni', false), sRes', 'uni', false);
-		
+
 		% replace empty response times with NaNs.
 		x	= cellfun(@(cx) cellfun(@(x) unless(x,{NaN}),cx,'uni',false),x,'uni',false);
-		
+
 		% unpack cells
 		tResponse  = cellfun(@(cx) cellfun(@(x) x{1}, cx),x,'uni',false);
 		tResponse  = vertcat(tResponse{:});
-		
+
 		% convert to ms
 		TR = PIParam.trTime;
 		sBlock.rt = TR * tResponse;
-		
-		
+
+
 		%%% Part 2: per-TR target and chunk information %%%
-		
+
 		% block2target parameters
 		HRF			= 1;
 		BlockOffset = opt.offset;
 		BlockSub	= opt.maxlen;
-				
-		% timings		
+
+		% timings
 		durBlock	= PIParam.exp.block.retention.time;
-		
+
 		durRest		= PIParam.exp.block.test.time		+ ...
 					  PIParam.exp.block.feedback.time	+ ...
 					  PIParam.exp.rest.time				+ ...
 					  PIParam.exp.block.prompt.time;
-				
+
 		durPre		= PIParam.exp.rest.time			+ ...
 					  PIParam.exp.block.prompt.time	- ...
 					  durRest;
-				  
+
 		durPost		= PIParam.exp.block.test.time		+ ...
 					  PIParam.exp.block.feedback.time	+ ...
 					  PIParam.exp.post.time				- ...
 					  durRest;
-				  
+
 		durRun		= PIParam.exp.run.time;
-		
+
 		% construct attributes
-		sAttr = struct;		
+		sAttr = struct;
 		nScheme = numel(cScheme);
 		cCondition = opt.ifo.cClass;
 		nCondition = numel(cCondition);
 		cConditionCI	= [repmat({'Blank'},[nCondition 1]); cCondition];
-		
+
 		for kS=1:nScheme
 			strScheme = cScheme{kS};
-			
+
 			nRun			= size(sBlock.(strScheme),1);
-					
+
 			% all blocks
 				[cTarget,cEvent]	= deal(cell(nRun,1));
 				for kR=1:nRun
@@ -184,30 +192,30 @@ function [sResult, bError] = LoadInfo(strSession, opt)
 					if kS==1
 						cEvent{kR}	= block2event(block,durBlock,durRest,durPre,durPost);
 					end
-					
+
 					% manual corrections
 					[cTarget{kR}, cEvent{kR}] = opt.fcorrect(strSession, kR, cTarget{kR}, cEvent{kR});
 				end
-				
+
 				sAttr.target.(strScheme).all = vertcat(cTarget{:});
-				
+
 				if kS==1
 					event		= eventcat(cEvent,durRun);
 					nEvent		= size(event,1);
 					durRunTotal	= durRun*nRun;
-					
+
 					event(:,1)	= 1:nEvent;
 					event(:,2)	= event(:,2) + HRF + BlockOffset;
 					event(:,3)	= BlockSub;
 					ev			= event2ev(event,durRunTotal);
-					
+
 					sAttr.chunk.all	= sum(ev.*repmat(1:nEvent,[durRunTotal 1]),2);
 				end
-				
+
 			% just correct blocks
 				[cTarget,cEvent]	= deal(cell(nRun,1));
 				for kR=1:nRun
-					
+
 					block		= sBlock.(strScheme)(kR,:);
 					correct		= sBlock.correct(kR,:);
 					blockCI		= block + nCondition*correct;
@@ -218,41 +226,41 @@ function [sResult, bError] = LoadInfo(strSession, opt)
 						'hrf',			HRF,			...
 						'block_offset',	BlockOffset,	...
 						'block_sub',	BlockSub		...
-						);					
+						);
 
 					% event matrix
 					if kS==1
 						cEvent{kR}	= block2event(blockCI,durBlock,durRest,durPre,durPost);
 					end
-					
+
 					% manual corrections
 					[cTarget{kR}, cEvent{kR}] = opt.fcorrect(strSession, kR, cTarget{kR}, cEvent{kR});
 				end
-					
+
 				sAttr.target.(strScheme).correct = vertcat(cTarget{:});
-					
+
 				if kS==1
 					event = eventcat(cEvent,durRun);
 					% remove incorrect events
 					event(event(:,1)<=nCondition,:) = [];
-					
+
 					nEvent		= size(event,1);
 					durRunTotal	= durRun*nRun;
-					
+
 					event(:,1)	= 1:nEvent;
 					event(:,2)	= event(:,2) + HRF + BlockOffset;
 					event(:,3)	= BlockSub;
 					ev			= event2ev(event,durRunTotal);
-					
+
 					sAttr.chunk.correct	= sum(ev.*repmat(1:nEvent,[durRunTotal 1]),2);
-				end				
+				end
 		end
-		
+
 		sResult = struct(...
 			'block'	, sBlock	, ...
 			'attr'	, sAttr		  ...
 			);
-		
+
 	else % data file for the session not found.
 		bError = true;
 		% return empty struct
